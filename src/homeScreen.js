ac6988f509bc79fd378b4a4634740cda92b87c96
MariaClaraOliveira08 <159197888+MariaClaraOliveira08@users.noreply.github.com>
2025-09-01 13:03:48 -0300

    01_09_2025

--- src/homeScreen.js
+++ src/homeScreen.js
@@ -1,19 +1,42 @@
 import React from "react";
 import { View, Button } from "react-native";
-import {useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import api from "./axios/axios";
 
 const HomeScreen = () => {
-    const navigation = useNavigation();
-    const handleCam= () => {
-        navigation.navigate("Cam");
+  const [stateLED, setStateLED] = useState({ value: false });
+  const navigation = useNavigation();
+  const handleCam = () => {
+    navigation.navigate("Cam");
+  };
+
+  async function ledToggle() {
+    try {
+      //faz requisição da api do adafruit
+      // ! negação ou seja se ta true fica false e assim por diante
+      const response = await api.toggleLED({ value: `${!stateLED.value}` });
+      setStateLED({value: !stateLED.value})
+      console.log("Resposta: ", response.data);
+    } catch (error) {
+      console.log("Erro", error.response.data);
     }
+  }
 
-    return(
-        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-            <Button title="Abrir camera" onPress={handleCam} color='blue'/>
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ marginBottom: 20 }}>
+        <Button title="Abrir camera" onPress={handleCam} color="blue" />
+      </View>
+      <View>
+        <Button
+          title={stateLED.value ? "Desligar LED" : "Ligar LED"}
+          onPress={ledToggle}
+          color={stateLED.value ? "red" : "green"}
+        />
+      </View>
+    </View>
+  );
+};
 
-}
-
-export default HomeScreen
+export default HomeScreen;
